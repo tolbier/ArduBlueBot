78a0ceef32abf161f3222ec6c0e5b8bebe079147
Lluis Toyos <toyos@parda.korama.org>
2014-05-19 15:16:58 +0200

    servo en 7pasos

--- src/auto_mod.ino
+++ src/auto_mod.ino
@@ -3,6 +3,7 @@
 #define AUTO_ROBOT_MODE 1
 #define BT_ROBOT_MODE 2
 #define SERVO_POSITIONS_LENGTH 7
+#define delay4servoStep 250
 int servo_positions[SERVO_POSITIONS_LENGTH]={5,30,60,90,120,150,173};
 
 Servo servo1;
@@ -10,29 +11,34 @@ int robotMode = BT_ROBOT_MODE;
 
 void loop_auto_mod(){
   if (robotMode==AUTO_ROBOT_MODE){
-    scan_distance();
+    getNextDistance();
     stopMotors();
   }
 }
+
 void setup_auto_mod(){
   servo1.attach(10);
   Serial.print("setup_auto_mod");
 }
-
-void scan_distance(){
+void getNextDistance(){
   static unsigned long lasttime=millis();
-  if (millis()-lasttime > 500){
-    
-    int servo_position = next_servo_position();
-    moveServo(servo_position);
+  if (millis()-lasttime > delay4servoStep){
+    int idx_position=move_servo_next_position();
+    Serial.print (idx_position);
     lasttime=millis();
-  }  
+  }     
+}
+int move_servo_next_position(){
+    int idx_position;
+    int servo_position = next_servo_position(idx_position);
+    moveServo(servo_position);
+    return  idx_position;
 }
 
 void moveServo(int servo_position){
   servo1.write(servo_position);
 }
-int next_servo_position(){
+int next_servo_position(int &idx_position){
   static int servo_position_index=0;
   static int servo_step=1;
 
@@ -43,7 +49,7 @@ int next_servo_position(){
   }
   servo_position_index+=servo_step;
   int retorno= servo_positions[servo_position_index];
-
+  idx_position=servo_position_index;
   return retorno;
 } 
 
